08c32edcdbaa9b37e05a129865541be1fc55541e
Muninn <hyacinthus@gmail.com>
2016-01-06 18:08:48 +0800

    use os env to save settings

--- settings.py
+++ settings.py
@@ -1,21 +1,29 @@
+import os
+
+# flask listen port
+LISTEN_PORT = int(os.environ.get('COLD_PORT', 5000))
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-MONGO_USERNAME = ''
-MONGO_PASSWORD = ''
-MONGO_DBNAME = 'crandom'
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', '')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'crandom')
 
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
 # read-only access to the endpoint).
-#RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+RESOURCE_METHODS = ['GET', 'POST']
 
 # Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
+# page size
+PAGINATION_DEFAULT = 10
+
+# data
 joke = {
     # 'title' tag used in item links.
     'item_title': 'Dry humor',
