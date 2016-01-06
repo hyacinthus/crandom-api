7c29aaa44a93302082438a7b5c36b4a65c1be188
Muninn <hyacinthus@gmail.com>
2016-01-06 18:07:45 +0800

    custom listen port

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from eve import Eve
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.crandom
+
 app = Eve()
 
+
+@app.route('/random/', defaults={'count': 1})
+@app.route('/random/<int:count>')
+def random(count):
+    pass
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port = app.config.get("LISTEN_PORT"))
--- toolkit/insert.py
+++ toolkit/insert.py
@@ -4,8 +4,8 @@ client = MongoClient()
 db = client.crandom
 
 joke = {
-    "content": "有一天小强问他爸爸：“爸爸，我是不是傻孩子啊？”爸爸说：“傻孩子，你怎么会是傻孩子呢？”",
-    # "answer": "",
+    "content": "有一只熊走过来，猜一个成语。",
+    "answer": "有备而来。",
     # "author": "",
     "via": "豆瓣",
     # "via_url": "http://jandan.net/",
