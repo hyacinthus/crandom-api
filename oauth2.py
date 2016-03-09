4639efe35dbcf0e871fdfb092ce50e07f04069b1
Muninn <hyacinthus@gmail.com>
2016-03-09 17:43:26 +0800

    add oauth getter and setter

--- oauth2.py
+++ oauth2.py
@@ -1,5 +1,5 @@
-import uuid
 from eve.auth import BasicAuth
+from datetime import datetime, timedelta
 from flask import request
 from redis import StrictRedis
 from flask_oauthlib.provider import OAuth2Provider
@@ -10,14 +10,14 @@ import settings
 
 oauth = OAuth2Provider()
 mongo = Connection(host=settings.MONGO_HOST,
-                        port=settings.MONGO_PORT)
+                   port=settings.MONGO_PORT)
 redisdb = StrictRedis(db=9)
 
 
-## 认证数据结构
+# 认证数据结构
 @mongo.register
 class User(Document):
-    """A user, or resource owner, 
+    """A user, or resource owner,
     is usually the registered user on your site."""
     __collection__ = 'users'
     __database__ = 'crandom'
@@ -28,6 +28,7 @@ class User(Document):
             "username": str,
             "password": str,
     }
+
 
 @mongo.register
 class Client(Document):
@@ -53,7 +54,7 @@ class Client(Document):
 
 
 class Grant():
-    """A grant token is created in the authorization flow, 
+    """A grant token is created in the authorization flow,
     and will be destroyed when the authorization finished.
     new empty grant: Grant()
     get exist grant: Grant(client_id,code)"""
@@ -73,7 +74,6 @@ class Grant():
             self.user_id = self._get("user_id")
             self.expires = self._get("expires")
 
-
     def _get(self, key):
         return redisdb.get("oauth:grant:%s:%s:%s" % (self.client_id,
                                                      self.code,
@@ -81,19 +81,195 @@ class Grant():
 
     def _set(self, key, value):
         redisdb.set("oauth:grant:%s:%s:%s" % (self.client_id,
-                                                     self.code,
-                                                     key)),
+                                              self.code,
+                                              key),
                     value)
 
+    def _del(self, key):
+        redisdb.delete("oauth:grant:%s:%s:%s" % (self.client_id,
+                                                 self.code, key))
+
     def save(self):
-        if client_id and code:
+        if self.client_id and self.code:
             self._set("redirect_uri", self.redirect_uri)
             self._set("scopes", self.scopes)
             self._set("user_id", self.user_id)
             self._set("expires", self.expires)
 
+    def delete(self):
+        if self.client_id and self.code:
+            self._del("redirect_uri")
+            self._del("scopes")
+            self._del("user_id")
+            self._del("expires")
 
-## 校验认证部分
+
+class Token():
+    """ bearer token is the final token
+    that could be used by the client.
+    new empty token: Token()
+    get exist token: Token(access_token)
+    get by refresh token: Token(refresh_token=refresh_token)"""
+    access_token = None
+    refresh_token = None
+    client_id = None
+    scopes = None
+    user_id = None
+    expires = None
+
+    def __init__(self, access_token=None, refresh_token=None):
+        if access_token:
+            self.access_token = access_token
+            self.refresh_token = self._geta("refresh_token")
+            self.client_id = self._geta("client_id")
+            self.scopes = self._geta("scopes")
+            self.user_id = self._geta("user_id")
+            self.expires = self._geta("expires")
+        elif refresh_token:
+            self.refresh_token = refresh_token
+            self.access_token = self._getr("access_token")
+            self.client_id = self._getr("client_id")
+            self.scopes = self._getr("scopes")
+            self.user_id = self._getr("user_id")
+            self.expires = self._getr("expires")
+
+    def _geta(self, key):
+        return redisdb.get("oauth:access_token:%s:%s" %
+                           (self.access_token, key))
+
+    def _getr(self, key):
+        return redisdb.get("oauth:refresh_token:%s:%s" %
+                           (self.refresh_token, key))
+
+    def _dela(self, access_token):
+        redisdb.delete("oauth:access_token:%s:%s" %
+                       (access_token, self.refresh_token))
+        redisdb.delete("oauth:access_token:%s:%s" %
+                       (access_token, self.client_id))
+        redisdb.delete("oauth:access_token:%s:%s" %
+                       (access_token, self.scopes))
+        redisdb.delete("oauth:access_token:%s:%s" %
+                       (access_token, self.user_id))
+        redisdb.delete("oauth:access_token:%s:%s" %
+                       (access_token, self.expires))
+
+    def _delr(self, refresh_token):
+        redisdb.delete("oauth:refresh_token:%s:%s" %
+                       (refresh_token, self.access_token))
+        redisdb.delete("oauth:refresh_token:%s:%s" %
+                       (refresh_token, self.client_id))
+        redisdb.delete("oauth:refresh_token:%s:%s" %
+                       (refresh_token, self.scopes))
+        redisdb.delete("oauth:refresh_token:%s:%s" %
+                       (refresh_token, self.user_id))
+        redisdb.delete("oauth:refresh_token:%s:%s" %
+                       (refresh_token, self.expires))
+
+    def save(self):
+        # one token per one client one user
+        old_access = redisdb.get("oauth:user:%s:%s" %
+                                 (self.user_id, self.client_id))
+        if old_access and old_access != self.access_token:
+            old_refresh = redisdb.get(
+                    "oauth:access_token:%s:refresh_token" %
+                    old_access)
+            if old_refresh:
+                self._delr(old_refresh)
+            self._dela(old_access)
+        # save user index
+        if self.client_id and self.user_id:
+            redisdb.set("oauth:user:%s:%s" %
+                        (self.user_id, self.client_id),
+                        self.access_token)
+        # save access token
+        redisdb.set("oauth:access_token:%s:refresh_token" %
+                    self.access_token, self.refresh_token)
+        redisdb.set("oauth:access_token:%s:client_id" %
+                    self.access_token, self.client_id)
+        redisdb.set("oauth:access_token:%s:scopes" %
+                    self.access_token, self.scopes)
+        redisdb.set("oauth:access_token:%s:user_id" %
+                    self.access_token, self.user_id)
+        redisdb.set("oauth:access_token:%s:expires" %
+                    self.access_token, self.expires)
+        # save refresh token
+        redisdb.set("oauth:refresh_token:%s:access_token" %
+                    self.refresh_token, self.access_token)
+        redisdb.set("oauth:refresh_token:%s:client_id" %
+                    self.refresh_token, self.client_id)
+        redisdb.set("oauth:refresh_token:%s:scopes" %
+                    self.refresh_token, self.scopes)
+        redisdb.set("oauth:refresh_token:%s:user_id" %
+                    self.refresh_token, self.user_id)
+        redisdb.set("oauth:refresh_token:%s:expires" %
+                    self.refresh_token, self.expires)
+
+    def delete(self):
+        if self.refresh_token:
+            self._delr(self.refresh_token)
+        if self.access_token:
+            self._dela(self.access_token)
+        redisdb.delete("oauth:user:%s:%s" %
+                       (self.user_id, self.client_id))
+
+
+# 注册认证函数
+@oauth.clientgetter
+def load_client(client_id):
+    return mongo.Client.find({"client_id": client_id})
+
+
+@oauth.grantgetter
+def load_grant(client_id, code):
+    return Grant(client_id, code)
+
+
+@oauth.grantsetter
+def save_grant(client_id, code, request, *args, **kwargs):
+    expires = datetime.utcnow() + timedelta(seconds=100)
+    grant = Grant()
+    grant.client_id = client_id
+    # todo 这里不理解
+    grant.code = code['code']
+    grant.redirect_uri = request.redirect_uri
+    grant.scopes = request.scopes
+    grant.user_id = request.user._id
+    grant.expires = expires
+    grant.save()
+    return grant
+
+
+@oauth.tokengetter
+def load_token(access_token=None, refresh_token=None):
+    if access_token:
+        return Token(access_token=access_token)
+    elif refresh_token:
+        return Token(refresh_token=refresh_token)
+
+
+@oauth.tokensetter
+def save_token(token, request, *args, **kwargs):
+    expires_in = token.get('expires_in')
+    expires = datetime.utcnow() + timedelta(seconds=expires_in)
+    tok = Token()
+    tok.access_token = token['access_token']
+    tok.refresh_token = token['refresh_token']
+    tok.client_id = request.client.client_id
+    tok.scopes = token['scope']
+    tok.user_id = request.user._id
+    tok.expires = expires
+    tok.save()
+    return tok
+
+
+@oauth.usergetter
+def get_user(username, password, *args, **kwargs):
+    user = mongo.User.find_one({"username": username,
+                                "password": password})
+    return user
+
+
+# 校验认证部分
 class BearerAuth(BasicAuth):
     """ Overrides Eve's built-in basic authorization scheme and uses Redis to
     validate bearer token
@@ -124,6 +300,3 @@ class BearerAuth(BasicAuth):
         except:
             token = None
         return self.check_auth(token, allowed_roles, resource, method)
-
-
-
